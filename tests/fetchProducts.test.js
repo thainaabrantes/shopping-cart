9bea868aca95922ad929675631a271478cbb69db
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-10-14 18:23:54 -0300

    Requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,23 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it ('fetchProducts deve ser uma função', () => {
+    expect(typeof fetchProducts).toEqual('function');
+  })
+  it ('fetch deve ser chamada ao passar o argumento computador para a função fetchProducts', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHavebeenCalled();
+  })
+  it ('fetch deve utilizar o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador" com o argumento "computador"', async () => {
+    await fetchProducts('computador');
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  })
+  it ('o retorno de fetchProducts com o argumento "computador" deve ser igual ao objeto computadorSearch', async () => {
+    const returnedObject = await fetchProducts('computador');
+    expect(returnedObject).toEqual(computadorSearch);
+  })
+  it ('fetchProducts deve retornar um erro quando chamada sem argumentos',  async () => {
+    expect(await fetchProducts()).toThrow(new Error('You must provide an url!'));
+  })
 });
